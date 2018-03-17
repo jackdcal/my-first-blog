938d05c370c7245083edb100121ff77416440b96
jackdcal <jackdcal@gmail.com>
2018-03-17 20:42:48 +0000

    add changes and new view post_detail

--- flimp/urls.py
+++ flimp/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     #url(r'^$', views.post_list, name='post_list'),
-    path('', views.post_list, name='post_list')
+    path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    #path(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail')
 ]
